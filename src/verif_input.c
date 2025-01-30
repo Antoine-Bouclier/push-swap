1deebbdaab0ca0199d7fff00cc073faee839cb29
Antoine-Bouclier <135241983+Antoine-Bouclier@users.noreply.github.com>
2025-01-30 16:06:53 +0100

    Adding verif for the input

---
+++ src/verif_input.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verif_input.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 09:57:54 by abouclie          #+#    #+#             */
+/*   Updated: 2025/01/30 12:49:46 by abouclie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+
+static int	arg_is_number(char *argv)
+{
+	int	i;
+
+	i = 0;
+	while (argv[i])
+	{
+		if (argv[i] == '-' || argv[i] == '+')
+			i++;
+		if (ft_isdigit(argv[i]) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+static int	verif_duplicates(char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while(argv[i])
+	{
+		j = i + 1;
+		while (argv[j])
+		{
+			if (ft_strcmp(argv[i], argv[j]) == 0)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	is_correct_arg(char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (argv[i])
+	{
+		if (!arg_is_number(argv[i]))
+			return (0);
+		i++;
+	}
+	if (!verif_duplicates(argv))
+		return (0);
+	return (1);
+}
---
+++ src/verif_input_utils.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verif_input_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abouclie <abouclie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 10:53:21 by abouclie          #+#    #+#             */
+/*   Updated: 2025/01/30 12:23:22 by abouclie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	ft_isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]))
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
